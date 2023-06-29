5ed34992599964f95444ee3a4ed2e2a7d5c7160c
정윤환 <109188479+Torychu@users.noreply.github.com>
2023-06-29 13:33:55 +0900

    Add files via upload

--- CloneCoding/NOMAD/JS/chrome_app/2.13/app.js
+++ CloneCoding/NOMAD/JS/chrome_app/2.13/app.js
@@ -8,9 +8,15 @@ const age = parseInt(prompt("How old are you?"));
 //age가 NaN(Not a Nomber)이 아니면 false가 나옴
 //age가 NaN이면 true가 나옴
 
-if (isNaN(age)) {
+if (isNaN(age) || age < 0) {
   //이렇게 작성할 경우 isNaN을 통해 조건문이 true와 false로 반환되므로 if문의 실행여부를 결정할 수 있음
-  console.log("Please write a number");
-} else {
-  console.log("Thank you for writing your age.");
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("Yon can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 }
